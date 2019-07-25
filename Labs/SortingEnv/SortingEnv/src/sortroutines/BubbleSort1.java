c595b63e661630cb4a3728ae9bdad01944d96fd1
Sujiv Shrestha <sujivshrestha@yahoo.com>
2019-07-25 01:27:00 -0500

    Lab3 homework added with resources

---
+++ Labs/SortingEnv/SortingEnv/src/sortroutines/BubbleSort1.java
@@ -0,0 +1,56 @@
+package sortroutines;
+
+import java.util.Arrays;
+
+import runtime.Sorter;
+
+/**
+ * This is the slowest version of BubbleSort
+ * No optimization for already sorted arrays.
+ * Doesn't take into account the fact that
+ * largest elements are pushed to the right.
+ *
+ */
+public class BubbleSort1 extends Sorter {
+	
+	int[] arr;
+	public int[] sort(int[] array){
+		this.arr = array;
+		bubbleSort();
+		return arr;
+		
+	}
+	private void bubbleSort(){
+		
+		int len = arr.length;
+		for(int i = 0; i < len; ++i) {
+			//System.out.println(Arrays.toString(arr));
+			for(int k=0;k < len-1;k++) {
+				if(arr[k]>arr[k+1])
+					break;
+				return;
+			}
+			for(int j = 0; j < len-1; ++j) {
+				if(arr[j]> arr[j+1]){
+					swap(j,j+1);
+				}
+			}
+		}
+	}
+	
+	int[] swap(int i, int j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		return arr;
+		
+	}
+	public static void main(String[] args){
+		int[] test = {21,13,1,-22, 51, 5, 18};
+		BubbleSort1 bs = new BubbleSort1();
+		
+		System.out.println(Arrays.toString(bs.sort(test)));
+		
+	}
+
+}
